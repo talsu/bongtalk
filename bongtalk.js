a72f6933e9f03788d7c3bfd97e9c6d243db45c2e
talsu <talsu84@gmail.com>
2013-12-09 16:12:44 +0900

    rewrite bin script

--- bongtalk.js
+++ bongtalk.js
@@ -9,80 +9,87 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var util = require('util');
-var argv = require('optimist')
-    .usage('Usage : $0 --port [num]')
-    .demand(['port'])
-    .default({port:3000})
-    .argv;
 
-var servicePort = argv.port;
+exports.BongTalk = (function () {
+    function BongTalk(servicePort) {
+        this.servicePort = servicePort;
+    }
 
-
-var app = express();
+    BongTalk.prototype.start = function () {
+        var app = express();
 
 // all environments
-app.set('port', process.env.PORT || servicePort);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+        app.set('port', process.env.PORT || this.servicePort);
+        app.set('views', path.join(__dirname, 'views'));
+        app.set('view engine', 'jade');
+        app.use(express.favicon());
+        app.use(express.logger('dev'));
+        app.use(express.json());
+        app.use(express.urlencoded());
+        app.use(express.methodOverride());
+        app.use(app.router);
+        app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
-if ('development' === app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-var server = http.createServer(app);
-
-var io = require('socket.io').listen(server);
-
-server.listen(app.get('port'), function () {
-    util.log('Express server listening on port ' + app.get('port'));
-});
-
-var models = require('./models');
-var User = models.User;
-var Zones = models.Zones;
-
-/**
- * Global variables
- */
-var zones = new Zones();
-
-io.sockets.on('connection', function (socket) {
-    //create user
-    var user = new User(socket, socket.id);
-
-    //sendProfile
-    socket.emit('sendProfile', user.getSimpleUser());
-
-    socket.on('joinZone', function(data) {
-        user.name = data.user.name;
-        zones.getZone(data.zoneId).join(user);
-    });
-
-    socket.on('sendMessage', function(data){
-        if (user.currentZone) {
-            user.currentZone.broadcastTalk(user, data);
+        if ('development' === app.get('env')) {
+            app.use(express.errorHandler());
         }
-    });
 
-    socket.on('changeName', function(data){
-        user.changeName(data);
-    });
+        app.get('/', routes.index);
+        app.get('/users', user.list);
 
-    socket.on('disconnect', function(){
-        util.log(" Peer " + user.name + " disconnected.");
-        if (user.currentZone) {
-            user.currentZone.leave(user);
-        }
-    });
-});
+        var server = http.createServer(app);
+
+        var io = require('socket.io').listen(server);
+
+        server.listen(app.get('port'), function () {
+            util.log('Express server listening on port ' + app.get('port'));
+        });
+
+        var models = require('./models');
+        var User = models.User;
+        var Zones = models.Zones;
+
+        /**
+         * Global variables
+         */
+        var zones = new Zones();
+
+        io.sockets.on('connection', function (socket) {
+            //create user
+            var user = new User(socket, socket.id);
+
+            //sendProfile
+            socket.emit('sendProfile', user.getSimpleUser());
+
+            socket.on('joinZone', function(data) {
+                user.name = data.user.name;
+                zones.getZone(data.zoneId).join(user);
+            });
+
+            socket.on('sendMessage', function(data){
+                if (user.currentZone) {
+                    user.currentZone.broadcastTalk(user, data);
+                }
+            });
+
+            socket.on('changeName', function(data){
+                user.changeName(data);
+            });
+
+            socket.on('disconnect', function(){
+                util.log(" Peer " + user.name + " disconnected.");
+                if (user.currentZone) {
+                    user.currentZone.leave(user);
+                }
+            });
+        });
+    };
+
+    return BongTalk;
+})();
+
+
+
+
+
