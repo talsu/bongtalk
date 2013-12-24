9640bb8684be2b51bdaca2117c61ab6ab896a0ce
talsu <talsu84@gmail.com>
2013-12-24 16:41:49 +0900

    fix user kick bug.

--- public/javascripts/status.js
+++ public/javascripts/status.js
@@ -3,11 +3,34 @@
  */
 "use strict";
 
+;!(function ($) {
+    $.fn.classes = function (callback) {
+        var classes = [];
+        $.each(this, function (i, v) {
+            var splitClassName = v.className.split(/\s+/);
+            for (var j in splitClassName) {
+                var className = splitClassName[j];
+                if (-1 === classes.indexOf(className)) {
+                    classes.push(className);
+                }
+            }
+        });
+        if ('function' === typeof callback) {
+            for (var i in classes) {
+                callback(classes[i]);
+            }
+        }
+        return classes;
+    };
+})(jQuery);
+
 $(function () {
     $(".btn-user-kick").click(function(){
-        var userId = $(this).attr('id');
+        var userId = $(this).data('user');
+        var channelId = $(this).data('channel');
+
         $.ajax({
-            url: '/api/kick?channel=' + 'default' + '&user=' + userId,
+            url: '/api/kick?channel=' + channelId + '&user=' + userId,
             async:true
         })
         .done(function(data){
